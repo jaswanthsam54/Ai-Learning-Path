5ea0c166dbe92dde36703cf5eccd08577f9d9b5c
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2026-05-12 12:59:01 +0530

    Add simple interest calculation script

---
+++ Python/p1.9.py
@@ -0,0 +1,8 @@
+# Q9: Calculate simple interest using Principal, Rate, and Time.
+P = float(input("Enter Principal: "))
+R = float(input("Enter Rate: "))
+T = float(input("Enter Time: "))
+
+SI = (P * R * T) / 100
+
+print("Simple Interest:", SI)
